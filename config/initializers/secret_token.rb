0b874b98479c797addc00c52ab4dc7edcf4ca59c
Drantum <webmaster@prime-selection.eu>
2010-09-19 10:09:56 +0200

    Ruby 3 Source

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SchweinemastManagementSystem::Application.config.secret_token = '0c3735e269a03da2727433acff5ddae91f2ce95e89f9bb15bfd804a07292994f60f8fcda85c68fd58c66eb436d49c77c55cd872962bcb982ef55d56745f7f304'
